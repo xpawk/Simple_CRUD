a1c8ba97635874099b7485e63f355ad87bb4f6c8
xpawk <xpawk@o2.pl>
2023-06-01 17:08:10 +0200

    remove refresh after page change

--- frontend/static/js/routing.js
+++ frontend/static/js/routing.js
@@ -2,30 +2,43 @@ import Events from './subPages/adminPanel/events.js';
 import AdminPanel from './subPages/adminPanel/adminPanel.js';
 import Home from './subPages/home.js';
 
-const router = async () => {
+const routes = [
+    {
+        view: AdminPanel,
+        path: '/adminPanel',
+        additionalClasses: [Events],
+    },
+    { view: Home, path: '/', additionalClasses: [] },
+];
+
+const router = async (path = '/') => {
     try {
-        const routes = [
-            {
-                view: AdminPanel,
-                path: '/adminPanel',
-                additionalClasses: [Events],
-            },
-            { view: Home, path: '/', additionalClasses: [] },
-        ];
-        routes.forEach(async (el) => {
-            if (el.path === window.location.pathname) {
-                const view = await new el.view();
+        for (const route of routes) {
+            if (route.path === path) {
+                const view = await new route.view();
                 document.querySelector('#app').innerHTML = await view.getHtml();
-                el.additionalClasses.forEach(async (classInstance) => {
-                    await new classInstance();
-                });
+                for (const Class of route.additionalClasses) {
+                    await new Class();
+                }
+                break;
             }
-        });
+        }
     } catch (error) {
         console.log(error);
     }
 };
 
+const handleNavigation = (e) => {
+    if (e.target.matches('a')) {
+        e.preventDefault();
+        const path = e.target.getAttribute('href');
+        history.pushState(null, '', path);
+        router(path);
+    }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-    router();
+    document.body.addEventListener('click', handleNavigation);
+    window.addEventListener('popstate', () => router(window.location.pathname));
+    router(window.location.pathname);
 });
